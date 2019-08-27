ad1fd7281d6c2b34273eabb22df0de7eeee690f3
wanwh <wanweihua_1@163.com>
2019-08-27 16:25:13 +0800

    增加spring boot admin 发送微信模板消息功能

--- springcloud-learning-admin/src/main/java/com/wanwh/springcloudlearningadmin/config/MyNotifier.java
+++ springcloud-learning-admin/src/main/java/com/wanwh/springcloudlearningadmin/config/MyNotifier.java
@@ -40,10 +40,8 @@ public class MyNotifier extends AbstractStatusChangeNotifier {
     private static final String DEFAULT_MESSAGE = "*#{instance.registration.name}* (#{instance.id}) is *#{event.statusInfo.status}**";
 
     private final SpelExpressionParser parser = new SpelExpressionParser();
-    private RestTemplate restTemplate = new RestTemplate();
+//    private RestTemplate restTemplate = new RestTemplate();
     private String wxOpenIds;
-    private String atMobiles;
-    private String msgtype = "markdown";
     private String title = "服务告警";
     private Expression message;
     private String url = "http://127.0.0.1:8084/xlzx/api/sendWeChatInfo.action";
@@ -55,43 +53,13 @@ public class MyNotifier extends AbstractStatusChangeNotifier {
         this.message = parser.parseExpression(DEFAULT_MESSAGE, ParserContext.TEMPLATE_EXPRESSION);
     }
 
-    private String getMessage(InstanceEvent event,Instance instance) {
-        Map<String, Object> root = new HashMap<>(3);
-        root.put("event", event);
-        root.put("instance", instance);
-        root.put("lastStatus", getLastStatus(event.getInstance()));
-        StandardEvaluationContext context = new StandardEvaluationContext(root);
-        context.addPropertyAccessor(new MapAccessor());
-        return message.getValue(context, String.class);
-    }
-
-    private String getAtMobilesString(String s) {
-        StringBuilder atMobiles = new StringBuilder();
-        String[] mobiles = s.split(",");
-        for (String mobile : mobiles) {
-            atMobiles.append("@").append(mobile);
-        }
-        return atMobiles.toString();
-    }
-
-    private HttpEntity<String> createMessage(InstanceEvent event, Instance instance) {
+    private HttpEntity<String> createMessage() {
         HttpHeaders headers = new HttpHeaders();
         //一定要设置好ContentType为utf8否则会乱码
         MediaType type = MediaType.parseMediaType("application/json; charset=UTF-8");
         headers.setContentType(type);
         headers.add("Accept", MediaType.APPLICATION_JSON.toString());
-        Map<String, Object> messageJson = new HashMap<>(4);
-        List<Map<String,String>> openIdList = new ArrayList<Map<String,String>>(1);
-        openIdList.add(new HashMap<String, String>(){
-            {
-                put("weixin_id", wxOpenIds);
-            }
-        });
-        messageJson.put("openId", JSONUtil.toJsonStr(openIdList));
-        messageJson.put("content", Util.getWeChatTemplate("服务监控狗巡检","","","",""));
-        messageJson.put("templateId",templateId);
-        messageJson.put("tokenId",MD5Util.md5Hex(templateId + "2018zktj"));
-        return new HttpEntity(messageJson, headers);
+        return new HttpEntity("", headers);
     }
 
     private String creatParams(InstanceEvent event, Instance instance){
@@ -103,7 +71,7 @@ public class MyNotifier extends AbstractStatusChangeNotifier {
             }
         });
         String parmas = "templateId=" + templateId +
-                "&content=" + JSONUtil.toJsonStr(Util.getWeChatTemplate("服务监控狗巡检","2","3",instance.getInfo().getValues().get("project-name")+"",instance.getStatusInfo().getDetails().get("message") + "")) +
+                "&content=" + JSONUtil.toJsonStr(Util.getWeChatTemplate(title,"","",instance.getInfo().getValues().get("project-name")+"",instance.getStatusInfo().getDetails().get("message") + "")) +
                 "&openId=" + JSONUtil.toJsonStr(openIdList) +
                 "&tokenId=" + MD5Util.md5Hex(templateId + salt);
         return parmas;
